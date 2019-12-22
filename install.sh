9359ac8d0ba2ea79f027809f30934df135e371e1
Patsakula Nikita <nikita.patsakula@gmail.com>
2019-12-22 15:04:23 +0700

    [*] {CI} Minor fixed: typos, format.

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 # Update repos + install update:
-sudo apt update && sudo apt dist-upgrade -y
+ sudo apt update && sudo apt dist-upgrade -y
 
 # Add wireguard repository:
 sudo apt-apt-repository ppa:wireguard/wireguard
 sudo apt update
 
 # Install system and cpp dependencies:
-sudo apt install -y htop curl wireguard ranger neovim zsh build-essential clang valgrind
+sudo apt install -y \
+    htop curl autoconf \
+    wireguard ranger neovim \
+    zsh build-essential clang valgrind
 
 # Install ctags:
 sudo apt install -y libjansson-dev
@@ -15,13 +18,13 @@ cd ctags && ./autogen.sh && ./configure && make && sudo make install
 
 # Install zsh + install zshrc:
 curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -fsSL | sh
-echo "export ZSH=$(echo ~/.oh-my-zsh)\n$(cat .zshrc)" > ~/.zshrc
+echo "export ZSH=$(echo ~/.oh-my-zsh)\n$(cat ./.zshrc)" > ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 chsh -s $(which zsh)
 
 # Copy vim and ranger configs:
-cp -r ./ranger ~/.config
-cp -r ./nvim ~/.comfig
+cp -r ./ranger/ ~/.config/
+cp -r ./nvim/ ~/.config/
 
 # Install rust and haskell toolkit:
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
