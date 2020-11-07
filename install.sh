66c03e3e69eec5cee0bb6fe59548d5fea4e1eacd
Patsakula Nikita <nikita.patsakula@gmail.com>
2020-11-07 15:06:34 +0300

    [*] Install script improved.

--- install.sh
+++ install.sh
@@ -2,20 +2,31 @@
 sudo pacman-mirrors --geoip && sudo pacman -Syyu
 
 # Update repos:
-sudo pacman -Syu
+sudo pacman -Syyu
 
-# 'linux-headers' is deps for 'wireguard-dkms'
-sudo pacman -S linux-headers wireguard-tools wireguard-dkms zsh \
-    neovim ranger ctags bash-language-server rustup stack clang \
-    tokei ripgrep sd bat fd exa
+# Install apps:
+sudo pacman -S \
+    # Required for WG dkms:
+    linux-headers \
+    # VPN tools:
+    wireguard-tools wireguard-dkms \
+    # Shell:
+    zsh \
+    # File manager:
+    ranger \
+    # C/C++ compiler + language server:
+    clang \
+    # Bash language server:
+    bash-language-server \
+    # Rust toolchain manager:
+    rustup \
+    # Text editor + LSC:
+    kakoune kak-lsp
+    # System tools:
+    ripgrep bat exa fd tokei flameshot tokei
 
 # For some reason '.cargo/bin' is not added.
 export PATH=~/.cargo/bin:$PATH
-
-rustup toolchain default stable
-rustup toolchain add nightly
-rustup component add rust-src rustfmt-preview clippy-preview rls
-cargo install navi
 
 # Install zsh + install .zshrc:
 curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -fsSL | sh
@@ -23,7 +34,7 @@ echo "export ZSH=$(echo ~/.oh-my-zsh) \n\n$(cat ./.zshrc)" > ./.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 chsh -s $(which zsh)
 
-# Copy vim and ranger configs:
-rm -R ~/.config/ranger/ && rm -R ~/.config/nvim/ && rm ~/.zshrc
-ln -s "$(pwd)/ranger" ~/.config && ln -s "$(pwd)/nvim" ~/.config 
-ln -s "$(pwd)/.zshrc" ~/
+ln -s "$(pwd)/nvim" ~/.config
+ln -s "$(pwd)/kak" ~/.config
+ln -s "$(pwd)/kak-lsp" ~/.config
+rm ~/.zshrc && ln -s "$(pwd)/.zshrc" ~/
